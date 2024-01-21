abaa04bc98e78eaf7fcabbc8e87fa27735fc2671
cedric hamilton <cedricjhamilton@gmail.com>
2024-01-21 18:10:30 -0500

    updated variables and local storage

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 var apikey = "bf38229c443f86d1aebdddd765b527c1";
 
 // initializing variables
-var formEl = document.getElementById('form')
 var searchInput = document.getElementById('Search_input');
 var searchBtn = document.getElementById('search_button');
 var currentCity = document.getElementById('current_city');
@@ -16,24 +15,90 @@ var weekHumidity = document.getElementById('week_humidity');
 var weekWind = document.getElementById('week_wind');
 var stored = [];
 
-if (localStorage.getItem("past_city_searches")) {
-    var past_city_searches = JSON.parse(localStorage.getItem('past_city_searches'));
+if (localStorage.getItem("pastCitySearches")) {
+    var pastCitySearches = JSON.parse(localStorage.getItem('pastCitySearches'));
 } else {
-    var past_city_searches = [];
+    var pastCitySearches = [];
 }
 // fuctions
 function getSearch(event) {
 
     var currentDate = moment().format("M/D/YYYY");
-    var coordsURL = https://api.openweathermap.org/data/3.0/weather?q=${searchInput.value}&appid={bf38229c443f86d1aebdddd765b527c1}
-        console.log(coordsURL);
+    var coordsURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apikey}`
+        ;
+    console.log(coordsURL);
     fetch(coordsURL)
         .then(function (response) {
             return response.json();
         })
-    .then(function (response) {
-    return response.json();
-})
-    .then(function (localStorage) {
-        console.log(localStorage);
-    });
+        .then(function (locData) {
+            console.log(locData);
+            var cityLat = locData.coord.lat;
+            var cityLon = locData.coord.lon;
+            var part = "minutely, hourly, alerts";
+            var urlQuery = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apikey}"
+        });
+    fetch(urlQuery)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            var iconWeather = locData.weather[0].icon;
+            var iURL = "http://openweathermap.org/img/w/" + iconWeather + ".png";
+            $('#weather-icon').attr('src', iURL);
+
+            currentCity.textContent = locData.name + " (" + currentDate + ")";
+            currentTemp.textContent = "Temp: " + (((parseInt(locData.main.temp) - 273.15) * 1.80) + 32).toFixed(2)
+                + "°F";
+            currentWind.textContent = "Wind: " + data.current.wind_speed + "MPH";
+            currentHumidity.textContent = "Humidity: " + data.current.humidity + "%";
+
+            var pastCity = locData.name;
+            pastCitySearches.push(pastCity)
+            localStorage.setItem("pastCitySearches", JSON.stringify(pastCitySearches));
+
+
+            for (var i = 0; i < 5; i++) {
+                var minTemp = data.daily[i].temp.min;
+                var maxTemp = data.daily[i].temp.max;
+                var dayHumidity = data.daily[i].humidity;
+                var dayWind = data.daily[i].wind_speed;
+                var dayDate = moment().add(i + 1, "days").format("M/D/YYYY");
+                var iconCode = data.daily[i].weather[0].icon;
+                var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+                $('#img' + i).attr('src', iconURL);
+
+                document.getElementById('week_date' + i).textContent = dayDate;
+                document.getElementById('week_tempMax' + i).textContent = "Max Temp: " + (((parseInt(maxTemp) - 273.15) * 1.80) + 32).toFixed(2) + "°F";
+                document.getElementById('week_tempMin' + i).textContent = "Min Temp: " + (((parseInt(minTemp) - 273.15) * 1.80) + 32).toFixed(2) + "°F";
+                document.getElementById('week_wind' + i).textContent = "wind: " + dayWind + "MPH";
+                document.getElementById('week_humidity' + i).textContent = "Humidity: " + dayHumidity + "%";
+
+            }
+        })
+
+
+};
+
+function renderPastSearch() {
+    var stored = JSON.parse(localStorage.getItem("pastCitySearches"));
+    console.log("PAST", stored);
+    document.getElementById("Past_City").innerHTML = '';
+
+    for (var i = 0; i < stored.length; i++) {
+        var pastSearchE1 = document.createElement('input');
+        pastSearchE1.setAttribute('type', 'text');
+        pastSearchE1.setAttribute('readonly', true);
+        pastSearchE1.setAttribute('class',);
+        pastSearchE1.setAttribute('value', stored[i]);
+        pastSearchE1.addEventListener('click', function () {
+            getSearch(pastCitySearches.value);
+        });
+        document.getElementById("Past_City").append(pastSearchE1);
+    };
+}
+// event listeners
+$("#search_button").on("click", getSearch);
